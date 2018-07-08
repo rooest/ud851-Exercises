6e0df98b9b96d23d2e7581390a27804d1ee45d97
Ömer Aksu <omeraksu@teknasyon.com>
2018-07-08 15:53:40 +0300

    - Completed

--- app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.todolist;
 
@@ -115,13 +115,21 @@ public class AddTaskActivity extends AppCompatActivity {
         int priority = getPriorityFromViews();
         Date date = new Date();
 
-        // TODO (4) Make taskEntry final so it is visible inside the run method
-        TaskEntry taskEntry = new TaskEntry(description, priority, date);
-        // TODO (2) Get the diskIO Executor from the instance of AppExecutors and
+        // Completed (4) Make taskEntry final so it is visible inside the run method
+        final TaskEntry taskEntry = new TaskEntry(description, priority, date);
+        // Completed (2) Get the diskIO Executor from the instance of AppExecutors and
         // call the diskIO execute method with a new Runnable and implement its run method
-        // TODO (3) Move the remaining logic inside the run method
-        mDb.taskDao().insertTask(taskEntry);
-        finish();
+        AppExecutors.getInstance().diskIO()
+                .execute(new Runnable() {
+                             @Override
+                             public void run() {
+                                 // COmpleted (3) Move the remaining logic inside the run method
+                                 mDb.taskDao().insertTask(taskEntry);
+                                 finish();
+                             }
+                         }
+                );
+
     }
 
     /**
